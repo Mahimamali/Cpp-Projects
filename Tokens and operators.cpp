27288d7cb8cdc3a47d4b0624f97036f6cbd3d5f5
Mahimamali <65186929+Mahimamali@users.noreply.github.com>
2020-06-10 22:33:49 +0530

    7. Tokens and operators

---
+++ Tokens and operators.cpp
@@ -0,0 +1,26 @@
+///1. Sizes of data types
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"Size of char: "<<size of (char)<<" byte"<<endl;
+  cout<<"Size of int: "<<size of (int)<<" byte"<<endl;
+  cout<<"Size of float: "<<size of (float)<<" byte"<<endl;
+  cout<<"Size of double: "<<size of (double)<<" byte"<<endl;
+  return 0;
+}
+
+
+///2. Product of numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout<<"Enter two numbers:"<<endl;
+cin>>a>>b;
+c=a*b;
+cout<<"Product="<<c;
+}
